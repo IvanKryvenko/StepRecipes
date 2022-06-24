bf12e496922493e70b5723004b92c90531e69888
Ivan Kryvenko <ivan.kryv14@gmail.com>
2022-06-24 16:14:38 +0300

    dev/SR-02

--- srback/src/app.module.ts
+++ srback/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, RecipeModule],
   controllers: [AppController],
   providers: [AppService],
 })
