df352e18b42413a1ebf1ee0eda8a303b4a07fd31
Ivan Kryvenko <ivan.kryv14@gmail.com>
2022-07-12 17:41:15 +0300

    initialize work with errors handlers and add file with error messages

---
+++ srback/src/errors.ts
@@ -0,0 +1,5 @@
+export const Errors: any {
+    UNKNOWN_USER = `This user doesn't exist in the system`,
+    USER_ALREADY_EXIST_NICKNAME = `User with this nickname already exists in the system`,
+    USER_ALREADY_EXIST_EMAIL = `User with this email already exists in the system`,
+}
