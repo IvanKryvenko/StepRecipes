e256b326e5319a771b839fc4f954622ab4d132d1
Alexandra  Vereshchagina <volflyubitevumurmurmur@gmail.com>
2022-07-15 23:08:39 +0300

    fix

--- srfront/src/app/modules/HomeModule/home.module.ts
+++ srfront/src/app/modules/HomeModule/home.module.ts
@@ -5,8 +5,8 @@ import { RouterModule } from '@angular/router'
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { RecipeListComponent} from 'src/app/components/recipe-list/recipe-list.component';
 import { CardRecipeComponent } from 'src/app/components/card-recipe/card-recipe.component';
-import { RecipeListComponent } from 'src/app/components/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
