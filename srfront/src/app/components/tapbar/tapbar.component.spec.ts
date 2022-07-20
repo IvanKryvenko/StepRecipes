55b9f6612722f2d115f8b7836e203999ffbdea03
Alexandra  Vereshchagina <volflyubitevumurmurmur@gmail.com>
2022-07-20 20:11:39 +0300

    create tapbar component

---
+++ srfront/src/app/components/tapbar/tapbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TapbarComponent } from './tapbar.component';
+
+describe('TapbarComponent', () => {
+  let component: TapbarComponent;
+  let fixture: ComponentFixture<TapbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TapbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TapbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- srfront/src/app/modules/MainModule/main-routing.module.ts
+++ srfront/src/app/modules/MainModule/main-routing.module.ts
@@ -1,5 +1,8 @@
+import { WishListComponent } from './../../components/wish-list/wish-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavouriteComponent } from 'src/app/components/favourite/favourite.component';
+import { SearchComponent } from 'src/app/components/search/search.component';
 import { MainComponent } from './main.component';
 
 const routes: Routes = [
@@ -20,7 +23,21 @@ const routes: Routes = [
         path: 'user',
         loadChildren: () => import('../UserModule/user.module').then(m => m.UserModule),
       },
-
+      {
+        path: 'search',
+        component: SearchComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'favourite',
+        component: FavouriteComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'wish',
+        component: WishListComponent,
+        pathMatch: 'full'
+      }
     ]
   },
 ];
